542cb40cefc42400031a698e119ef7de0964a35d
Håkon Pettersen <hakon.helgetun.pettersen@schibsted.com>
2024-01-23 11:37:31 +0100

    Add jitpack.yml

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -12,16 +12,16 @@ kotlin {
         }
     }
     
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "shared"
-            isStatic = true
-        }
-    }
+//    listOf(
+//        iosX64(),
+//        iosArm64(),
+//        iosSimulatorArm64()
+//    ).forEach {
+//        it.binaries.framework {
+//            baseName = "shared"
+//            isStatic = true
+//        }
+//    }
 
     sourceSets {
         commonMain.dependencies {
